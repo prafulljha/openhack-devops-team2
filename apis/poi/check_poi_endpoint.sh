284f9da6582f4f3c4163bffdf40640a9f20106ab
Prafull Jha <prafull.jha@gmail.com>
2021-01-26 21:12:10 +0000

    Externalise health check endpoint

--- apis/poi/check_poi_endpoint.sh
+++ apis/poi/check_poi_endpoint.sh
@@ -3,15 +3,16 @@ declare -i duration=10
 declare hasUrl=""
 declare endpoint
 declare -i status200count=0
-endpoint="https://openhacku787but6poi-staging.azurewebsites.net/api/healthcheck/poi"
+#endpoint="https://openhacku787but6poi-staging.azurewebsites.net/api/healthcheck/poi"
 healthcheck() {
     declare url=$1
     result=$(curl -i $url 2>/dev/null | grep HTTP/2)
     echo $result
 }
+echo 'Polling endpoint: ' $ENDPOINT
 for i in {1..12}
 do
-  result=`healthcheck $endpoint`
+  result=`healthcheck $ENDPOINT`
   echo "---> $result" 
   declare status
   if [[ -z $result ]]; then 
@@ -23,7 +24,7 @@ do
     if [[ -z $hasUrl ]]; then
       echo "$timestamp | $status "
     else
-      echo "$timestamp | $status | $endpoint " 
+      echo "$timestamp | $status | $ENDPOINT " 
     fi 
     echo "status=$status"
     if [ $status -eq 200 ]; then
