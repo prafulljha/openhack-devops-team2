8c70f9048717222a04fd194e9c4e64fcefd97ad5
Prafull Jha <prafull.jha@gmail.com>
2021-01-27 10:56:03 +0000

    Fixed missed ;

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -9,7 +9,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Healthy; Build Id: " //+ buildId;
+            Status = "Healthy; Build Id: "; //+ buildId;
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
